450ef14e274a4c82e13ab978aac3257ffd61f257
shashankyadava <shashankyadava08@gmail.com>
2024-10-26 01:32:55 +0530

    added the new schema and used the joins

--- DataBase.py
+++ DataBase.py
@@ -1,32 +1,57 @@
 import sqlite3
 
-#connect to the database
+def create_tables():
+    #connect to the database
 
-conn = sqlite3.connect('shoes.db') # if shoes.db does not exist it will create it
-cursor = conn.cursor() # it will point to the specific database
+    conn = sqlite3.connect('shoes.db') # if shoes.db does not exist it will create it
+    cursor = conn.cursor() # it will point to the specific database
 
-#create table or schema
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS nike (
-        id INTEGER PRIMARY KEY,
-        name TEXT NOT NULL
-    )
-''')
+    #create table or schema
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS nike (
+            nike_id INTEGER PRIMARY KEY,
+            model TEXT NOT NULL,
+            size INTEGER,
+            color TEXT,
+            type TEXT,
+            price REAL
+        )
+    ''')
 
-#create second table or schema
-cursor.execute(''' 
-    CREATE TABLE IF NOT EXISTS adidas (
-        id INTEGER PRIMARY KEY,
-        name TEXT NOT NULL
-    )
-''')
+    #create second table or schema
+    cursor.execute(''' 
+        CREATE TABLE IF NOT EXISTS adidas (
+            adidas_id INTEGER PRIMARY KEY,
+            model TEXT NOT NULL,
+            size INTEGER,
+            color TEXT,
+            type TEXT,
+            price REAL
+        )
+    ''')
 
-#insert sample data into both tables
-cursor.execute("INSERT INTO nike(id,name) VALUES(1,'jordan')")
-cursor.execute("INSERT INTO adidas(id,name) VALUES(2,'sambas')")
-conn.commit()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS shoes_list (
+            shoes_id INTEGER PRIMARY KEY,
+            shoe_name TEXT NOT NULL,
+            adidas_id INTEGER,
+            nike_id INTEGER,
+            FOREIGN KEY (adidas_id) REFERENCES adidas(adidas_id),
+            FOREIGN KEY (nike_id) REFERENCES nike(nike_id)   
+        )
+    ''')
 
-#close the connection
-conn.close()
+
+
+
+
+    conn.commit()
+
+    #close the connection
+    conn.close()
+
+if __name__ == '__main__':
+    create_tables()
+    
 
 
