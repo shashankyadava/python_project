52842c2c14764d408fc650e8ee7800b7a2c752a0
shashankyadava <shashankyadava08@gmail.com>
2024-11-13 08:37:31 +0530

    made the post api to post shoes

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from pkgutil import get_data
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import sqlite3
 
 class Server:
@@ -13,6 +13,7 @@ class Server:
 
     def add_routes(self):
         self.__server.add_url_rule('/shoes', '/shoes', self.get_shoes, methods=['GET'])
+        self.__server.add_url_rule('/post_shoes', '/post_shoes', self.post_shoes, methods=['POST'])
 
     @staticmethod
     def __get_db_connection():
@@ -49,6 +50,67 @@ class Server:
 
         conn.close()
         return jsonify(data)
+    
+    def post_shoes(self):
+        conn = self.__get_db_connection()
+        cursor = conn.cursor()
+
+        #     cursor.execute('''
+        #     CREATE TABLE IF NOT EXISTS shoes (
+        #         shoes_id INTEGER PRIMARY KEY,
+        #         brand TEXT NOT NULL,
+        #         shoe_name TEXT NOT NULL,
+        #         shape TEXT NOT NULL,
+        #         size INT NOT NULL,
+        #         menwear TEXT NOT NULL,
+        #         womanwear TEX NOT NULL,
+        #         price INT NOT NULL,
+        #         manufacturer TEXT NOT NULL      
+        #     )
+        # ''')
+        data = request.get_json()
+        #print(data)
+        brand = data['brand']
+        #print(brand)
+        shoe_name = data['shoe_name']
+        #print(shoe_name)
+        shape = data['shape']
+        #print(shape)
+        size = data['size']
+        #print(size)
+        menwear = data['menwear']
+        #print(menwear)
+        womanwear = data['womanwear']
+        #print(womanwear)
+        price = data['price']
+        #print(price)
+        manufacturer = data['manufacturer']
+        #print(manufacturer)
+
+        checklist = {
+            "brand" : brand,
+            "shoe_name" : shoe_name,
+            "shape" : shape,
+            "size" : size,
+            "menwear" : menwear,
+            "womanwear" : womanwear,
+            "price" : price,
+            "manufacturer" : manufacturer
+        }
+
+        for shoeinfo, shoevalue in checklist.items():
+            if not shoevalue:
+                #print(shoevalue)
+                return jsonify({"error":shoeinfo+" "+"is required"})
+        
+        cursor.execute("INSERT INTO shoes(brand,shoe_name,shape,size,menwear,womanwear,price,manufacturer) VALUES(?,?,?,?,?,?,?,?)", (brand,shoe_name,shape,size,menwear,womanwear,price,manufacturer))
+        conn.commit()
+        return jsonify({"message":"shoe added successfully"})
+
+        
+
+
+
     
     
 
