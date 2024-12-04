96f066631e07faedc97b6995366ec198b0bd3848
shashankyadava <shashankyadava08@gmail.com>
2024-12-05 04:43:43 +0530

    made the patch api for id and name

--- server.py
+++ server.py
@@ -15,6 +15,7 @@ class Server:
         self.__server.add_url_rule('/shoes', '/shoes', self.get_shoes, methods=['GET'])
         self.__server.add_url_rule('/create_shoes', '/create_shoes', self.create_shoes, methods=['POST'])
         self.__server.add_url_rule('/remove_shoe', '/remove_shoe', self.remove_shoe, methods=['DELETE'])
+        self.__server.add_url_rule('/update_shoe', '/update_shoe', self.update_shoe, methods=['PATCH'])
 
     # @staticmethod
     # def __get_db_connection():
@@ -86,6 +87,32 @@ class Server:
         return database.remove_data(query,params)
 
 
+    def update_shoe(shelf):
+        try:
+            shoe_id = request.args.get('shoe_id')
+            shoe_name = request.args.get('shoe_name')
+            update_data = request.get_json()
+            # Dynamically generate the SET clause from the request data
+            set_clause = ", ".join([f"{key} = ?" for key in update_data.keys()])
+            
+
+            # Prepare the UPDATE query
+            
+            if not update_data:
+                return jsonify({'error': 'No data provided'}), 400
+            
+            if shoe_id and shoe_name:
+                query = f'''UPDATE shoes SET {set_clause} WHERE shoes_id = ? AND shoe_name = ?'''
+                params = list(update_data.values()) + [shoe_id, shoe_name]
+            elif shoe_id:
+                query = f'''UPDATE shoes SET {set_clause} WHERE shoes_id = ?'''
+                params = list(update_data.values()) + [shoe_id]
+            else:
+                return jsonify({"message":"shoe_id or shoe_name is required"})
+            
+        except sqlite3.Error as e:
+            return e
+        return database.update_shoe(query,params)
 
 
     
